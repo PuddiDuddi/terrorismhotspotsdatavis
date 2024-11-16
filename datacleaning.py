fde50cadfa2f4064f9a939f14466a79cd85d94a3
Puddi <evilgerm@gmail.com>
2024-11-16 19:39:42 +0100

    refactor, dropping dlt for snowflake official connector

--- datacleaning.py
+++ datacleaning.py
@@ -1,35 +1,40 @@
 import pandas as pd
 
-df = pd.read_csv("terrorismcsv.csv")
-pd.set_option("display.max_columns", None)
-df[["imonth", "iday"]] = df[["imonth", "iday"]].replace(0, 1)
-df["date"] = pd.to_datetime(
-    df[["iyear", "imonth", "iday"]].rename(
-        columns={"iyear": "year", "imonth": "month", "iday": "day"}
+
+def clean_data():
+    print("Cleaning data...")
+    df = pd.read_csv("terrorismcsv.csv")
+    pd.set_option("display.max_columns", None)
+    df[["imonth", "iday"]] = df[["imonth", "iday"]].replace(0, 1)
+    df["date"] = pd.to_datetime(
+        df[["iyear", "imonth", "iday"]].rename(
+            columns={"iyear": "year", "imonth": "month", "iday": "day"}
+        )
     )
-)
-df["date"] = df["date"].dt.strftime("%Y-%m-%d")
-usefulldata = df[
-    [
-        "eventid",
-        "date",
-        "country_txt",
-        "region_txt",
-        "provstate",
-        "city",
-        "latitude",
-        "longitude",
-        "success",
-        "suicide",
-        "attacktype1_txt",
-        "targtype1_txt",
-        "gname",
-        "nperps",
-        "weapsubtype1_txt",
-        "nkill",
-        "nkillter",
-        "nwound",
+    df["date"] = df["date"].dt.strftime("%Y-%m-%d")
+    usefuldata = df[
+        [
+            "eventid",
+            "date",
+            "country_txt",
+            "region_txt",
+            "provstate",
+            "city",
+            "latitude",
+            "longitude",
+            "success",
+            "suicide",
+            "attacktype1_txt",
+            "targtype1_txt",
+            "gname",
+            "weapsubtype1_txt",
+            "nkill",
+            "nkillter",
+            "nwound",
+        ]
     ]
-]
-usefulldata.fillna(value="null")
-usefulldata.to_csv("filteredterroristdata.csv", index=False)
+    usefuldata.loc[:, ["nkill", "nkillter", "nwound"]] = usefuldata.loc[:, ["nkill", "nkillter", "nwound"]].fillna(0)
+    usefuldata.loc[:, ["weapsubtype1_txt", "provstate", "city"]] = usefuldata.loc[:, ["weapsubtype1_txt", "provstate", "city"]].fillna("Unknown")
+    usefuldata = usefuldata.fillna("null")
+    usefuldata.to_csv("filteredterroristdata.csv", index=False)
+    print("Data cleaning completed.")
--- filesystem_pipeline.py
+++ filesystem_pipeline.py
@@ -1,27 +1,63 @@
-import dlt
+import snowflake.connector
+import os
+from datacleaning import clean_data
 
-
-from dlt.sources.filesystem import readers, read_csv
-
-
-
-# used for extracting data from source, in this case a local csv file 
-files = readers(bucket_url="filteredterroristdata.csv").read_csv()
-
-
-pipeline = dlt.pipeline(
-        pipeline_name="snowflakecsv",
-        destination="snowflake",
-        dataset_name="terrorismhotspots"  # schema
-)
-
-
-
-# print the data yielded from resource
-print(files)
-
-# run the pipeline with your parameters
-load_info = pipeline.run(files.with_name('filteredterroristdata.csv'), table_name="terrorismhotspots")
-
-    # pretty print the information on data that was loaded
-print(load_info)
+clean_data()
+file_path = os.path.join(os.getcwd(), "filteredterroristdata.csv")
+with snowflake.connector.connect(
+    connection_name="myconnection",
+) as conn:
+    with conn.cursor() as cur:
+        print(cur.execute("USE ROLE DLT_LOADER_ROLE").fetchall())
+        print(cur.execute("USE WAREHOUSE TERRORISMHOTSPOTS").fetchall())
+        print(cur.execute("USE DATABASE TERRORISMHOTSPOTS").fetchall())
+        print(cur.execute("USE SCHEMA TERRORISMHOTSPOTS").fetchall())
+        print(
+            cur.execute("""
+                CREATE OR REPLACE STAGE my_local_stage 
+                FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 0);
+                """).fetchall()
+        )
+        print(cur.execute(f"PUT file://{file_path} @my_local_stage").fetchall())
+        print(
+            cur.execute("""
+                CREATE OR REPLACE FILE FORMAT csv
+                TYPE = CSV
+                FIELD_DELIMITER = ','
+                SKIP_HEADER = 1
+                NULL_IF = ('NULL', 'null') 
+                EMPTY_FIELD_AS_NULL = true
+                COMPRESSION = gzip
+                FIELD_OPTIONALLY_ENCLOSED_BY = '0x22'
+                """).fetchall()
+        )
+        print(
+            cur.execute("""
+                CREATE OR REPLACE TABLE RAWINGEST (
+                eventid               number,
+                date                 date,
+                country_txt          text,
+                region_txt           text,
+                provstate            text,
+                city               text,
+                latitude            float,
+                longitude           float,
+                success               number,
+                suicide               number,
+                attacktype1_txt      text,
+                targtype1_txt        text,
+                gname                text,
+                weapsubtype1_txt     text,
+                nkill               number,
+                nkillter              number,
+                nwound              number  
+                )
+                """).fetchall()
+        )
+        print(
+            cur.execute("""
+                COPY INTO RAWINGEST
+                FROM @my_local_stage/filteredterroristdata.csv
+                FILE_FORMAT = (FORMAT_NAME = 'csv');
+                """).fetchall()
+        )
